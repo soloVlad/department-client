18ace85c5a6b50c8da9d435c0291679fa72623e7
Vlad Solovey <connectwithvlad@gmail.com>
2024-03-09 18:07:11 +0300

    ops: replace eslint with biome

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,12 @@ import App from "./App.tsx";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
+
+if (root) {
+	ReactDOM.createRoot(root).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
+}
