686c90aee57d3976779f89ab3c7227bdb101bbf4
Vlad Solovey <connectwithvlad@gmail.com>
2024-03-13 21:11:12 +0300

    feat: create welcome section layout

---
+++ src/assets/images/index.ts
@@ -0,0 +1,1 @@
+export { default as WelcomeBgImageUrl } from "./welcome-bg.png";
--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,7 +1,9 @@
 import type { FC } from "react";
-import { Container } from "@mantine/core";
+import { BackgroundImage, Container, Text, Title, rem } from "@mantine/core";
 
 import { Header } from "@/components";
+
+import { WelcomeBgImageUrl } from "@/assets/images";
 
 import classes from "./Home.module.css";
 
@@ -11,6 +13,22 @@ const HomePage: FC = () => {
 			<Header />
 
 			<main style={{ flex: 1 }}>
+				<BackgroundImage
+					className={classes.bgContainer}
+					mih={670}
+					src={WelcomeBgImageUrl}
+				>
+					<Container className={classes.welcomeContainer}>
+						<Title order={1} maw={750}>
+							Веб-технологии и компьютерное моделирование
+						</Title>
+
+						<Text mt={20} fw={400} fz={rem(24)} lh={rem(32.4)}>
+							Сайт кафедры ММФ БГУ
+						</Text>
+					</Container>
+				</BackgroundImage>
+
 				<Container>Main</Container>
 			</main>
 
