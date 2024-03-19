4793a3fe1a9db74078a9039045efdf78d1d82e4a
Vlad Solovey <connectwithvlad@gmail.com>
2024-03-19 23:11:20 +0300

    ops: fix deployment with Browser router

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import biomePlugin from "vite-plugin-biome";
 
 export default defineConfig({
-	base: "/department-client/",
+	base: "./",
 	plugins: [
 		react(),
 		tsconfigPaths(),
