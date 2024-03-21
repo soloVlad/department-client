641ab991d26905f4cffdfbc0d1f1c7f8ae9a9327
Vlad Solovey <connectwithvlad@gmail.com>
2024-03-21 13:10:50 +0300

    feat: add List theme styles

---
+++ src/theme/components/List/List.ts
@@ -0,0 +1,8 @@
+import { List, rem } from "@mantine/core";
+
+export default List.extend({
+	defaultProps: {
+		fz: rem(18),
+		fw: 600,
+	},
+});
--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Container } from "./Container/Container";
+export { default as List } from "./List/List";
 export { default as Menu } from "./Menu/Menu";
 export { default as Paper } from "./Paper/Paper";
 export { default as Spoiler } from "./Spoiler/Spoiler";
